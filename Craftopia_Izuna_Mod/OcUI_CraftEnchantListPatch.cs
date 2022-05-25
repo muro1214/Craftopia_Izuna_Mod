b8b3d15e0daad656652031509a245bae8297709e
muro1214 <s.muro.1214@gmail.com>
2022-05-25 13:04:29 +0900

    initial commit

---
+++ Craftopia_Izuna_Mod/OcUI_CraftEnchantListPatch.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using Oc.Item.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Craftopia_Izuna_Mod
+{
+    [HarmonyPatch(typeof(OcUI_CraftEnchantList), "TryLockEnchant")]
+    public class TryLockEnchantPatch
+    {
+        public static void Postfix(ref OcUI_CraftEnchantList __instance)
+        {
+            __instance.PurchaseCost = 0;
+        }
+    }
+
+    [HarmonyPatch(typeof(OcUI_CraftEnchantList), "GetLockedEnchantID")]
+    public class GetLockedEnchantIDPatch
+    {
+        public static bool Prefix(ref OcUI_CraftEnchantList __instance, ref int[] __result)
+        {
+
+			int[] array = new int[4];
+			if (__instance._lockCount == 0)
+			{
+				__result = array;
+				return false;
+			}
+			IEnumerable<OcUI_CraftEnchantSheet> enumerable = from sheet in __instance.itemEnchantList
+															 where sheet.Chosen
+															 select sheet;
+			int num = 0;
+			foreach (OcUI_CraftEnchantSheet ocUI_CraftEnchantSheet in enumerable)
+			{
+				array[num] = ocUI_CraftEnchantSheet.EnchantID;
+				num++;
+				if (num >= 4)
+				{
+					__result = array;
+					return false;
+				}
+			}
+
+			__result = array;
+			return false;
+        }
+    }
+}
---
+++ Craftopia_Izuna_Mod/Patcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+
+namespace Craftopia_Izuna_Mod
+{
+    [BepInPlugin(MOD_NAME, DISPLAY_NAME, VERSION)]
+    public class Patcher : BaseUnityPlugin
+    {
+        public const String MOD_NAME = "net.muro1214.Craftpia_Izuna_Mod";
+        public const String DISPLAY_NAME = "Craftpia_Izuna_Mod";
+        public const String VERSION = "1.0.0";
+
+        public void Awake()
+        {
+            var harmony = new Harmony(DISPLAY_NAME);
+            harmony.PatchAll();
+        }
+    }
+}
