84dabb1b800288538367d6bc8005ce9ea9976f40
muro1214 <s.muro.1214@gmail.com>
2022-05-25 15:24:56 +0900

    簡単な動作チェックが完了したやつ

--- Craftopia_Izuna_Mod/OcUI_CraftEnchantListPatch.cs
+++ Craftopia_Izuna_Mod/OcUI_CraftEnchantListPatch.cs
@@ -2,31 +2,71 @@
 using Oc.Item.UI;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 
 namespace Craftopia_Izuna_Mod
 {
     [HarmonyPatch(typeof(OcUI_CraftEnchantList), "TryLockEnchant")]
     public class TryLockEnchantPatch
     {
-        public static void Postfix(ref OcUI_CraftEnchantList __instance)
+        public static bool Prefix(OcUI_CraftEnchantList __instance, OcUI_CraftEnchantSheet ___selectingSheet, ref int ____lockCount)
         {
-            __instance.PurchaseCost = 0;
-        }
+			if (___selectingSheet == null)
+			{
+				return false;
+			}
+
+			if (___selectingSheet.Chosen)
+			{
+				___selectingSheet.SwitchChoose(false);
+				____lockCount--;
+			}
+			else if (____lockCount < 4)
+			{
+				___selectingSheet.SwitchChoose(true);
+				____lockCount++;
+			}
+
+			__instance.Sort();
+			__instance.Refresh();
+			AccessTools.Method(__instance.GetType(), "RefreshPurchasePrice").Invoke(__instance, null);
+
+			return false;
+		}
     }
 
-    [HarmonyPatch(typeof(OcUI_CraftEnchantList), "GetLockedEnchantID")]
+	[HarmonyPatch(typeof(OcUI_CraftEnchantList), "Sort")]
+	public class SortPatch
+    {
+		public static void Postfix(OcUI_CraftEnchantSheet ___fixedEnchantSheet)
+        {
+			___fixedEnchantSheet.gameObject.SetActive(false);
+
+		}
+    }
+
+	[HarmonyPatch(typeof(OcUI_CraftEnchantList), "Refresh")]
+	public class RefreshPatch
+	{
+		public static void Postfix(TextMeshProUGUI ___price)
+		{
+			___price.text = "0";
+		}
+	}
+
+	[HarmonyPatch(typeof(OcUI_CraftEnchantList), "GetLockedEnchantID")]
     public class GetLockedEnchantIDPatch
     {
-        public static bool Prefix(ref OcUI_CraftEnchantList __instance, ref int[] __result)
+        public static bool Prefix(ref int[] __result, int ____lockCount, List<OcUI_CraftEnchantSheet> ___itemEnchantList)
         {
 
 			int[] array = new int[4];
-			if (__instance._lockCount == 0)
+			if (____lockCount == 0)
 			{
 				__result = array;
 				return false;
 			}
-			IEnumerable<OcUI_CraftEnchantSheet> enumerable = from sheet in __instance.itemEnchantList
+			IEnumerable<OcUI_CraftEnchantSheet> enumerable = from sheet in ___itemEnchantList
 															 where sheet.Chosen
 															 select sheet;
 			int num = 0;
