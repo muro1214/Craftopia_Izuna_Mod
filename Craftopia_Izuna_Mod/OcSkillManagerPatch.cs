66c9a6deaaf91679040300c433e16e693626717a
muro1214 <s.muro.1214@gmail.com>
2022-05-26 23:21:40 +0900

    例の機能を無効化

---
+++ Craftopia_Izuna_Mod/OcSkillManagerPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Oc.Skills;
+
+namespace Craftopia_Izuna_Mod
+{
+    [HarmonyPatch(typeof(OcSkillManager), "GameStartCheckSkillPoint")]
+    public class GameStartCheckSkillPointPatch
+    {
+        public static bool Prefix()
+        {
+            if (!Patcher.isEnabled_Player.Value)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
